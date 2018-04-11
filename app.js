402ac47bbdfd49bfe9711768ebbad53b8ecf4552
crespoter <davidchristiek@gmail.com>
2018-04-12 00:20:02 +0530

    started front end

---
+++ app.js
@@ -0,0 +1,49 @@
+﻿var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var debug = require('debug');
+var routes = require('./routes/index');
+
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error.pug', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error.pug', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.set('port', process.env.PORT || 80);
+
+var server = app.listen(app.get('port'), function () {
+    debug('Express server listening on port ' + server.address().port);
+});
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/', function (req, res) {
+    res.render('index.ejs');
+});
+
+router.get('/signup', function (req, res){
+    res.render('signup.ejs');
+});
+router.get('/findrestaurant', function (req, res) {
+    res.render('findRestaurant.ejs');
+});
+module.exports = router;
